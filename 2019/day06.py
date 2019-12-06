8760730a1ff82a953bec9c3f7ea313adf67c69ec
Adam Hammes <ahammes@cortexmedia.ca>
2019-12-06 16:07:07 -0500

    Delete unused methods

--- 2019/day06.py
+++ 2019/day06.py
@@ -62,39 +62,6 @@ class Graph:
             if is_connected:
                 yield self._nodes[index]
 
-    def incoming_neighbors(self, node):
-        node_index = self._index[node]
-        for i in range(len(self._nodes)):
-            is_connected = self._matrix[i][node_index]
-            if is_connected:
-                yield self._nodes[i]
-
-    def remove_node(self, node):
-        node_index = self._index[node]
-        # remove incoming edges
-        for node_edges in self._matrix:
-            del node_edges[node_index]
-
-        del self._matrix[node_index]
-        del self._nodes[node_index]
-        self._index = {node: i for i, node in enumerate(self._nodes)}
-
-    def topological_ordering(self):
-        g = Graph(self._edges)
-
-        ordering = []
-        while g.nodes():
-            no_incoming = g.root_nodes()
-            ordering += no_incoming
-            [g.remove_node(node) for node in no_incoming]
-
-        return ordering
-
-    def root_nodes(self):
-        return [
-            node for node in self.nodes() if not list(self.incoming_neighbors(node))
-        ]
-
     def distances_from(self, start_node="COM"):
         distances = {start_node: 0}
         queue = collections.deque([start_node])
@@ -110,29 +77,11 @@ class Graph:
 
         return distances
 
-    def distance(self, x, y):
-        distances = {x: 0}
-        queue = collections.deque([x])
-
-        while True:
-            node = queue.popleft()
-            distance = distances[node]
-
-            if node == y:
-                return distances[node]
-
-            unseen_neighbors = set(self.neighbors(node)) - distances.keys()
-            for neighbor in unseen_neighbors:
-                distances[neighbor] = distance + 1
-                queue.append(neighbor)
-
 
 class TestDay06(unittest.TestCase):
     def test_examples(self):
         self.assertEqual(42, part1(TEST_INPUT))
         self.assertEqual(4, part2(TEST_INPUT_2))
-
-    def test_part2(self):
 
     def test_regressions(self):
         self.assertEqual(140608, part1(get_input()))
