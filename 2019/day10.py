6d4779f99b8edbf9f384cc2b14a52f2b5fb75623
Adam Hammes <ahammes@cortexmedia.ca>
2019-12-14 14:41:54 -0500

    Day 10 part 2

--- 2019/day10.py
+++ 2019/day10.py
@@ -1,5 +1,6 @@
 import collections
 from fractions import Fraction
+import itertools
 import math
 
 
@@ -10,7 +11,7 @@ def safely_reduce(dx, dy):
         return dx / abs(dx), dy
 
     gcd = math.gcd(dx, dy)
-    return dx / gcd, dy / gcd
+    return dx // gcd, dy // gcd
 
 
 def get_input():
@@ -23,19 +24,38 @@ class Point(collections.namedtuple("Point", ["x", "y"])):
         dx, dy = p_other.x - self.x, p_other.y - self.y
         return safely_reduce(dx, dy)
 
+    def angle(self, p_other):
+        from math import atan2, pi
+
+        ox, oy = p_other
+        dx, dy = ox - self.x, self.y - oy
+
+        offset = math.pi
+        angle = atan2(dx, dy) * 180 / pi
+
+        return (angle + 360) % 360
+
+    def distance_to(self, p_other):
+        from math import sqrt
+
+        ox, oy = p_other
+        dx, dy = ox - self.x, oy - self.y
+
+        return sqrt(dx ** 2 + dy ** 2)
+
 
 def parse_input(coordinate_grid):
     points = set()
 
-    for x, row in enumerate(coordinate_grid.splitlines()):
-        for y, character in enumerate(row):
+    for y, row in enumerate(coordinate_grid.splitlines()):
+        for x, character in enumerate(row):
             if character == "#":
                 points.add(Point(x, y))
 
     return points
 
 
-def part1(points):
+def make_vector_map(points):
     vector_map = collections.defaultdict(set)
 
     for point in points:
@@ -43,9 +63,55 @@ def part1(points):
             if point != other_point:
                 vector_map[point].add(point.reduced_rise_run(other_point))
 
-    return max(len(point_set) for point_set in vector_map.values())
+    return vector_map
+
+
+def get_station(vector_map):
+    return max(vector_map.keys(), key=lambda p: len(vector_map[p]))
+
+
+def part1(points):
+    vector_map = make_vector_map(points)
+    station = get_station(vector_map)
+    return len(vector_map[station])
+
+
+def kill_asteroids(points):
+    vector_map = make_vector_map(points)
+    center = get_station(vector_map)
+
+    other_points = set(points) - {center}
+    angle_map = collections.defaultdict(list)
+    [angle_map[center.angle(point)].append(point) for point in other_points]
+
+    for p_list in angle_map.values():
+        p_list.sort(key=center.distance_to)
+
+    angle_map = {
+        angle: collections.deque(points) for angle, points in angle_map.items()
+    }
+
+    sorted_angles = list(sorted(angle_map.keys()))
+
+    for angle in itertools.chain(sorted_angles):
+        if not any(angle_map.values()):
+            return
+
+        points = angle_map[angle]
+        if not points:
+            continue
+
+        yield points.popleft()
+
+
+def part2(points):
+    asteroids = [asteroid for _, asteroid in zip(range(200), kill_asteroids(points))]
+
+    x, y = asteroids[-1]
+    return x * 100 + y
 
 
 if __name__ == "__main__":
     points = parse_input(get_input())
     print(part1(points))
+    print(part2(points))
--- 2019/tests/test_day10.py
+++ 2019/tests/test_day10.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day10 import parse_input, get_input, part1
+from day10 import *
 
 
 MY_EXAMPLE = """
@@ -34,10 +34,52 @@ EXAMPLE_2 = """
 .##.#..###
 ##...#..#.
 .#....####
-"""
+""".strip()
+
+EXAMPLE_3 = """
+#.#...#.#.
+.###....#.
+.#....#...
+##.#.#.#.#
+....#.#.#.
+.##..###.#
+..#...##..
+..##....##
+......#...
+.####.###.
+""".strip()
+
+
+LARGE_EXAMPLE = """
+.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##
+""".strip()
 
 
 class TestDay10(unittest.TestCase):
+    def run_get_station(self, _in):
+        points = parse_input(_in)
+        vector_map = make_vector_map(points)
+        return get_station(vector_map)
+
     def test_examples(self):
         self.assertEqual(6, part1(parse_input(MY_EXAMPLE)))
         self.assertEqual(2, part1(parse_input(MY_EXAMPLE_2)))
@@ -46,3 +88,38 @@ class TestDay10(unittest.TestCase):
 
     def test_regressions(self):
         self.assertEqual(269, part1(parse_input(get_input())))
+
+    def test_center_point(self):
+        self.assertEqual(Point(3, 4), self.run_get_station(EXAMPLE_1))
+        self.assertEqual(Point(5, 8), self.run_get_station(EXAMPLE_2))
+        self.assertEqual(Point(1, 2), self.run_get_station(EXAMPLE_3))
+        self.assertEqual(Point(11, 13), self.run_get_station(LARGE_EXAMPLE))
+
+    def test_kill_asteroids(self):
+        points = parse_input(LARGE_EXAMPLE)
+
+        asteroids = [
+            asteroid for _, asteroid in zip(range(299), kill_asteroids(points))
+        ]
+
+        kill_order = {
+            1: (11, 12),
+            2: (12, 1),
+            3: (12, 2),
+            10: (12, 8),
+            20: (16, 0),
+            50: (16, 9),
+            100: (10, 16),
+            199: (9, 6),
+            200: (8, 2),
+            201: (10, 9),
+        }
+
+        for order in sorted(kill_order.keys()):
+            asteroid = kill_order[order]
+            self.assertEqual(asteroid, asteroids[order - 1], order)
+
+    def test_regressions(self):
+        points = parse_input(get_input())
+        self.assertEqual(part1(points), 269)
+        self.assertEqual(part2(points), 612)
