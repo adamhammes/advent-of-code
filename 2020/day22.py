1b966b17c96c1cf00fbfe66ffc53a6ab1448492c
Adam Hammes <ahammes@cortexmedia.ca>
2020-12-30 12:42:59 -0600

    2020 Day 22 Part 2

--- 2020/day22.py
+++ 2020/day22.py
@@ -1,10 +1,16 @@
 from collections import deque
-from typing import *
+import enum
+from typing import Set, Tuple, Deque
 
 import lib
 
 Deck = Deque[int]
 SeenPositions = Set[Tuple[Tuple[int, ...], Tuple[int, ...]]]
+
+
+class Player(enum.Enum):
+    Player1 = enum.auto()
+    Player2 = enum.auto()
 
 
 def parse_input(raw: str) -> Tuple[Deck, Deck]:
@@ -16,18 +22,6 @@ def parse_input(raw: str) -> Tuple[Deck, Deck]:
     return player_1, player_2
 
 
-def play(player_1: Deck, player_2: Deck):
-    left, right = player_1.popleft(), player_2.popleft()
-
-    if left < right:
-        player_2 += sorted([left, right], reverse=True)
-    elif left > right:
-        player_1 += sorted([left, right], reverse=True)
-    else:
-        player_1.append(left)
-        player_2.append(right)
-
-
 def deck_code(deck: Deck) -> int:
     total = 0
     for i, card in enumerate(reversed(deck)):
@@ -36,19 +30,62 @@ def deck_code(deck: Deck) -> int:
     return total
 
 
+class Game:
+    def __init__(self, player_1: Deck, player_2: Deck):
+        self.player_1 = player_1
+        self.player_2 = player_2
+
+        self.seen_positions = set()
+
+    def winning_deck_code(self):
+        return deck_code(self.player_1 or self.player_2)
+
+    def current_position(self):
+        return tuple(self.player_1), tuple(self.player_2)
+
+    def recurse(self, num_p1: int, num_p2: int) -> "Game":
+        player_1 = deque(list(self.player_1)[:num_p1])
+        player_2 = deque(list(self.player_2)[:num_p2])
+
+        return Game(player_1, player_2)
+
+    def play(self, *, recursive: bool) -> Player:
+        while self.player_1 and self.player_2:
+            if self.current_position() in self.seen_positions:
+                return Player.Player1
+
+            self.seen_positions.add(self.current_position())
+
+            left, right = self.player_1.popleft(), self.player_2.popleft()
+
+            if recursive and len(self.player_1) >= left and len(self.player_2) >= right:
+                sub_game = self.recurse(left, right)
+                winner = sub_game.play(recursive=True)
+            else:
+                winner = Player.Player1 if left > right else Player.Player2
+
+            if winner == Player.Player2:
+                self.player_2 += [right, left]
+            else:
+                self.player_1 += [left, right]
+
+        return Player.Player1 if self.player_1 else Player.Player2
+
+
 def part_1(raw: str):
     player_1, player_2 = parse_input(raw)
-
-    while player_1 and player_2:
-        play(player_1, player_2)
-
-    winner = player_1 or player_2
-    return deck_code(winner)
+    game = Game(player_1, player_2)
+    game.play(recursive=False)
+    return game.winning_deck_code()
 
 
-def play_recursive(seen_positions: SeenPositions, player_1: Deck, player_2: Deck):
-    pass
+def part_2(raw: str):
+    player_1, player_2 = parse_input(raw)
+    game = Game(player_1, player_2)
+    game.play(recursive=True)
+    return game.winning_deck_code()
 
 
 if __name__ == "__main__":
     print(part_1(lib.get_input(22)))
+    print(part_2(lib.get_input(22)))
--- 2020/tests/test_day22.py
+++ 2020/tests/test_day22.py
@@ -16,6 +16,17 @@ Player 2:
 10
 """
 
+RECURSIVE_SAMPLE = """
+Player 1:
+43
+19
+
+Player 2:
+2
+29
+14
+"""
+
 
 def test_parse_input():
     player_1, player_2 = parse_input(SAMPLE_INPUT_1)
@@ -24,23 +35,17 @@ def test_parse_input():
     assert player_2 == deque([5, 8, 4, 7, 10])
 
 
-def test_play():
-    player_1, player_2 = parse_input(SAMPLE_INPUT_1)
-
-    play(player_1, player_2)
-
-    assert player_1 == deque([2, 6, 3, 1, 9, 5])
-    assert player_2 == deque([8, 4, 7, 10])
-
-    play(player_1, player_2)
-
-    assert player_1 == deque([6, 3, 1, 9, 5])
-    assert player_2 == deque([4, 7, 10, 8, 2])
-
-
 def test_deck_code():
     assert deck_code(deque([3, 2, 10, 6, 8, 5, 9, 4, 7, 1])) == 306
 
 
 def test_part_1():
     assert part_1(SAMPLE_INPUT_1) == 306
+    assert part_1(lib.get_input(22)) == 34005
+
+
+def test_part_2():
+    # check that this terminates
+    assert part_2(RECURSIVE_SAMPLE)
+
+    assert part_2(SAMPLE_INPUT_1) == 291
