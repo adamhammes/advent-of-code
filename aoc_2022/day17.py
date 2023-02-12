d52f2270805cc9bd522f64916af3b9b16405f72e
Adam Hammes <adam@hammes.io>
2023-02-12 11:24:38 -0600

    2022 Day 17 Part 2

--- aoc_2022/day17.py
+++ aoc_2022/day17.py
@@ -1,14 +1,10 @@
 import collections
-import itertools
 import typing
-from typing import Iterator
 
 import lib
 from lib import Point as P
 
 EXAMPLE = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
-
-actual = lib.get_input(17)
 
 shapes = {
     "horizontal_line": (P(2, 0), P(3, 0), P(4, 0), P(5, 0)),
@@ -18,127 +14,132 @@ shapes = {
     "block": (P(2, 0), P(2, 1), P(3, 0), P(3, 1)),
 }
 
+ordered_shapes = list(shapes.values())
+Shape = tuple[P, ...]
+
 
 class CacheKey(typing.NamedTuple):
-    points: frozenset[P]
-    shape: tuple[P, ...]
-    jet: str
+    rocks: frozenset[P]
+    jet_index: int
+    shape_index: int
 
 
-def print_shape(shape: list[P]):
-    for y in range(3, -1, -1):
-        for x in range(8):
-            is_rock = P(x, y) in shape
-            print(lib.PRINTABLE_SQUARE if is_rock else " ", end="")
-        print()
+class CacheState(typing.NamedTuple):
+    num_shapes_dropped: int
+    y_max: int
 
 
-def print_rocks(rocks: set[P]):
-    y_max = max(p.y for p in rocks)
+class Tunnel:
+    def __init__(self, jet_pattern: str, cache_height=40):
+        self.jet_pattern = jet_pattern.strip()
+        self.rocks = set(P(x, 0) for x in range(8))
 
-    for y in range(y_max + 1, -1, -1):
-        for x in range(8):
-            is_rock = P(x, y) in rocks
-            print(lib.PRINTABLE_SQUARE if is_rock else " ", end="")
+        self.jet_index = 0
+        self.num_shapes_dropped = 0
 
-        print()
+        self.y_max = 0
 
+        self.cache: dict[CacheKey, list[CacheState]] = collections.defaultdict(list)
+        self.cache_height = cache_height
+        self.last_cache_key = CacheKey(rocks=frozenset(), jet_index=0, shape_index=0)
 
-def calc_rock_movement(rocks: set[P], shape: list[P], jets: Iterator[str]) -> list[P]:
-    while True:
-        movement = {"<": P(-1, 0), ">": P(1, 0)}[next(jets)]
-        jetted_shape = [p.displace(*movement) for p in shape]
+    def get_next_shape(self) -> Shape:
+        shape = ordered_shapes[self.num_shapes_dropped % len(ordered_shapes)]
+        self.num_shapes_dropped += 1
+        return tuple(p.displace(0, self.y_max + 4) for p in shape)
 
-        new_shape = (
-            jetted_shape
-            if all(p.x in range(7) and p not in rocks for p in jetted_shape)
-            else shape
+    def get_next_jet(self) -> P:
+        jet = self.jet_pattern[self.jet_index]
+        self.jet_index += 1
+        self.jet_index %= len(self.jet_pattern)
+        return {"<": P(-1, 0), ">": P(1, 0)}[jet]
+
+    def shape_in_bounds(self, shape: Shape) -> bool:
+        return all(p.x in range(7) and p not in self.rocks for p in shape)
+
+    def drop_rock(self) -> Shape:
+        shape = self.get_next_shape()
+
+        while True:
+            jet_movement = self.get_next_jet()
+            windswept_rock = tuple(p.displace(*jet_movement) for p in shape)
+
+            shape = windswept_rock if self.shape_in_bounds(windswept_rock) else shape
+
+            dropped_shape = tuple(p.displace(0, -1) for p in shape)
+
+            if not self.shape_in_bounds(dropped_shape):
+                return shape
+
+            shape = dropped_shape
+
+    def add_to_collection(self, shape: Shape) -> list[CacheState]:
+        self.rocks.update(shape)
+        shape_y_max = max(p.y for p in shape)
+        self.y_max = max(shape_y_max, self.y_max)
+
+        y_min = max(1, self.y_max - self.cache_height)
+
+        previous_cache_rocks = self.last_cache_key.rocks
+        new_cache_rocks = set(shape).union(previous_cache_rocks)
+        new_cache_rocks = {
+            P(p.x, p.y - y_min)
+            for p in new_cache_rocks
+            if p.y in range(y_min, self.y_max + 1)
+        }
+
+        cache_key = CacheKey(
+            rocks=frozenset(new_cache_rocks),
+            jet_index=self.jet_index,
+            shape_index=self.num_shapes_dropped % len(ordered_shapes),
         )
 
-        dropped_shape = [p.displace(0, -1) for p in new_shape]
-        if any(p in rocks for p in dropped_shape):
-            return new_shape
+        cache_state = CacheState(
+            num_shapes_dropped=self.num_shapes_dropped, y_max=self.y_max
+        )
 
-        shape = dropped_shape
+        self.cache[cache_key].append(cache_state)
+        self.last_cache_key = cache_key
+        return self.cache[cache_key]
 
+    def simulate(self, num_iterations: int, allow_skip=True):
+        iterations = 0
+        time_has_skipped = not allow_skip
+        while iterations < num_iterations:
+            iterations += 1
+            dropped_shape = self.drop_rock()
+            cache_state = self.add_to_collection(dropped_shape)
 
-def simulate_rocks(rocks: set[P], jets: Iterator[str], num_iterations: int) -> set[P]:
-    ordered_shapes = list(shapes.values())
-    y_max = max(p.y for p in rocks)
+            if len(cache_state) < 3 or time_has_skipped:
+                continue
 
-    for i in range(num_iterations):
-        current_shape = ordered_shapes[i % len(ordered_shapes)]
-        current_shape = [p.displace(0, y_max + 4) for p in current_shape]
+            time_has_skipped = True
+            s2 = cache_state[1]
+            s3 = cache_state[2]
 
-        dropped_shape = calc_rock_movement(rocks, current_shape, jets)
-        shape_y_max = max(p.y for p in dropped_shape)
-        y_max = max(y_max, shape_y_max)
+            iteration_gap = s3.num_shapes_dropped - s2.num_shapes_dropped
+            y_max_gap = s3.y_max - s2.y_max
 
-        rocks.update(dropped_shape)
+            cycles_to_skip = (num_iterations - iterations) // iteration_gap
+            iterations += cycles_to_skip * iteration_gap
+            simulated_y_max = self.y_max + y_max_gap * cycles_to_skip
 
-    return rocks
-
-
-def part_2(raw: str, n=2022) -> int:
-    rocks = set(P(x, 0) for x in range(8))
-    raw = raw.strip()
-    jets = itertools.cycle(raw)
-
-    safety_factor = 10
-    cycle_length = safety_factor * len(raw) * len(shapes)
-    print(cycle_length)
-
-    cycled_rocks = simulate_rocks(rocks, jets, cycle_length)
-    max_y = max(r.y for r in cycled_rocks)
-
-    cut_off_rocks = {r for r in rocks if r.y > max_y - 100}
-
-    divided, modulo = n // cycle_length, n % cycle_length
-    height_adjustment = max_y * (divided - 1)
-
-    final_rocks = simulate_rocks(cut_off_rocks, jets, modulo)
-    return height_adjustment + max(r.y for r in final_rocks)
+            y_max_diff = simulated_y_max - self.y_max
+            self.rocks = set(P(p.x, p.y + y_max_diff) for p in self.rocks)
+            self.y_max = simulated_y_max
 
 
-def part_1(raw: str, n=2022) -> int:
-    rocks = set(P(x, 0) for x in range(8))
-    jets = itertools.cycle(raw.strip())
-    final_rocks = simulate_rocks(rocks, jets, n)
-    return max(r.y for r in final_rocks)
+def part_1(raw: str) -> int:
+    tunnel = Tunnel(raw)
+    tunnel.simulate(2022, allow_skip=True)
+    return tunnel.y_max
 
 
-def find_seen_states(raw: str) -> int:
-    max_size = len(raw) * len(shapes)
-    states: dict[frozenset[P], list[int]] = collections.defaultdict(list)
-
-    state_cache: dict[CacheKey, ]
-
-    rocks = set(P(x, 0) for x in range(8))
-    jets = itertools.cycle(raw.strip())
-
-
-    simulate_rocks(rocks, jets, 20_000)
-    rocks_by_altitude = collections.defaultdict(list)
-    for rock in rocks:
-        rocks_by_altitude[rock.y].append(rock)
-
-    for y in range(20_000):
-
-        points = list()
-        for y_min in range(y - max_size, y):
-            points += rocks_by_altitude[y_min]
-
-        finger_print = frozenset(p.displace(0, -y) for p in points)
-        states[finger_print].append(y)
-
-    repeats = [occ for occ in states.values() if len(occ) > 1][0]
-    print(repeats)
-    for a, b in lib.window(repeats[1:], 2):
-        a_val = part_1(raw, a)
-        b_val = part_1(raw, b)
-        print(b_val - a_val)
+def part_2(raw: str) -> int:
+    tunnel = Tunnel(raw)
+    tunnel.simulate(1_000_000_000_000, allow_skip=True)
+    return tunnel.y_max
 
 
 if __name__ == "__main__":
-    # print(part_1(lib.get_input(17)))
-    find_seen_states(EXAMPLE)
+    print(part_2(lib.get_input(17)))
--- aoc_2022/tests/test_day17.py
+++ aoc_2022/tests/test_day17.py
@@ -9,7 +9,5 @@ def test_part_1():
     assert part_1(EXAMPLE) == 3068
 
 
-# def test_part_2():
-#     for i in range(1, 10):
-#         iterations = i * 2738 * 2
-#         print(part_1(actual, iterations))
+def test_part_2():
+    assert part_2(EXAMPLE) == 1514285714288
