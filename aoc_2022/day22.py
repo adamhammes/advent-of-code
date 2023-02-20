c2619e3eacb362479a569cba98fdd7fc66fb9b6c
Adam Hammes <adam@hammes.io>
2023-02-19 18:28:31 -0600

    2022 Day 22 Part 2 🎉

--- aoc_2022/day22.py
+++ aoc_2022/day22.py
@@ -1,4 +1,5 @@
 import collections
+import dataclasses
 import enum
 import re
 import typing as t
@@ -7,7 +8,7 @@ import lib
 from lib import Point
 
 
-class Directions(enum.Enum):
+class Dir(enum.Enum):
     North = Point(0, -1)
     East = Point(1, 0)
     South = Point(0, 1)
@@ -22,6 +23,163 @@ class Contents(enum.Enum):
 
 Grid = [[Contents]]
 Instruction = int | t.Literal["R"] | t.Literal["L"]
+
+
+square_mapping = {
+    # fmt: off
+    Point(1, 0): 1,
+    Point(2, 0): 2,
+    Point(1, 1): 3,
+    Point(0, 2): 4,
+    Point(1, 2): 5,
+    Point(0, 3): 6,
+    # fmt: on
+}
+
+rev_square_mapping: dict[int, Point] = {
+    _id: coord for coord, _id in square_mapping.items()
+}
+
+
+@dataclasses.dataclass
+class Grove:
+    grid: Grid
+    instructions: list[Instruction]
+    current_position = Point(0, 0)
+
+    directions = collections.deque(
+        [
+            Dir.East,
+            Dir.South,
+            Dir.West,
+            Dir.North,
+        ]
+    )
+
+    def __post_init__(self):
+        self.current_position = Point(self.grid[0].index(Contents.Air), 0)
+
+    @property
+    def edge_size(self) -> int:
+        return len(self.grid[0]) // 3
+
+    def square_id(self, p: Point) -> t.Optional[int]:
+        adjusted_x = p.x // self.edge_size
+        adjusted_y = p.y // self.edge_size
+
+        return square_mapping.get(Point(adjusted_x, adjusted_y), None)
+
+    def wrap(self, position: Point, direction: Dir) -> tuple[int, Point, Dir]:
+        current_square_id = self.square_id(position)
+        cur = self.relative_position(position)
+        size = self.edge_size
+
+        match current_square_id, direction:
+            case 1, Dir.North:
+                return 6, Point(0, cur.x), Dir.East
+            case 1, Dir.West:
+                return 4, Point(0, size - cur.y - 1), Dir.East
+            case 2, Dir.North:
+                return 6, Point(cur.x, size - 1), Dir.North
+            case 2, Dir.East:
+                return 5, Point(size - 1, size - cur.y - 1), Dir.West
+            case 2, Dir.South:
+                return 3, Point(size - 1, cur.x), Dir.West
+            case 3, Dir.East:
+                return 2, Point(cur.y, size - 1), Dir.North
+            case 3, Dir.West:
+                return 4, Point(cur.y, 0), Dir.South
+            case 4, Dir.North:
+                return 3, Point(0, cur.x), Dir.East
+            case 4, Dir.West:
+                return 1, Point(0, size - cur.y - 1), Dir.East
+            case 5, Dir.East:
+                return 2, Point(size - 1, size - cur.y - 1), Dir.West
+            case 5, Dir.South:
+                return 6, Point(size - 1, cur.x), Dir.West
+            case 6, Dir.East:
+                return 5, Point(cur.y, size - 1), Dir.North
+            case 6, Dir.South:
+                return 2, Point(cur.x, 0), Dir.South
+            case 6, Dir.West:
+                return 1, Point(cur.y, 0), Dir.South
+            case _:
+                print("oop")
+
+    def relative_position(self, p: Point):
+        square_corner = rev_square_mapping[self.square_id(p)]
+        return p.displace(*square_corner.times(-self.edge_size))
+
+    def absolute_position(self, square_id: int, position: Point) -> Point:
+        square_position = rev_square_mapping[square_id].times(self.edge_size)
+        return square_position.displace(*position)
+
+    def rotate_to_direction(self, direction: Dir):
+        while self.directions[0] != direction:
+            self.directions.rotate(1)
+
+    def peek_step(self, position: Point, direction: Dir) -> tuple[Point, Dir]:
+        naive_position = position.displace(*direction.value)
+
+        if self.square_id(naive_position) is not None:
+            return naive_position, direction
+
+        new_square_id, relative_position, new_direction = self.wrap(position, direction)
+        absolute_position = self.absolute_position(new_square_id, relative_position)
+        return absolute_position, new_direction
+
+    def move(self, direction: Dir, distance: int) -> tuple[Point, Dir]:
+        num_steps_taken = 0
+        current_position = self.current_position
+        last_viable_position = current_position
+        last_viable_direction = direction
+
+        while num_steps_taken < distance:
+            current_position, direction = self.peek_step(current_position, direction)
+
+            cell: Contents = self.grid[current_position.y][current_position.x]
+
+            match cell:
+                case Contents.Air:
+                    last_viable_position = current_position
+                    last_viable_direction = direction
+                    num_steps_taken += 1
+                case Contents.Wall:
+                    return last_viable_position, last_viable_direction
+                case Contents.Empty:
+                    pass
+
+        return last_viable_position, last_viable_direction
+
+    def current_score(self) -> int:
+        facing_points = {
+            Dir.East: 0,
+            Dir.South: 1,
+            Dir.West: 2,
+            Dir.North: 3,
+        }[self.directions[0]]
+
+        return (
+            (self.current_position.y + 1) * 1_000
+            + (self.current_position.x + 1) * 4
+            + facing_points
+        )
+
+    def do_thing(self):
+        for instruction in self.instructions:
+            match instruction:
+                case int():
+                    new_position, new_direction = self.move(
+                        self.directions[0], instruction
+                    )
+                    self.current_position = new_position
+                    self.rotate_to_direction(new_direction)
+                case "R":
+                    self.directions.rotate(-1)
+                case "L":
+                    self.directions.rotate(1)
+
+        return self.current_score()
 
 
 def parse_input(raw: str) -> (Grid, [Instruction]):
@@ -44,69 +202,11 @@ def parse_input(raw: str) -> (Grid, [Instruction]):
         int(string) if string.isdecimal() else string for string in instructions
     ]
 
-    return grid, instructions
-
-
-def move(grid: Grid, current_position: Point, direction: Point, distance: int) -> Point:
-    height, width = len(grid), len(grid[0])
-    num_steps_taken = 0
-    last_viable_position = current_position
-
-    while num_steps_taken < distance:
-        current_position = current_position.displace(*direction)
-        x, y = current_position.x % width, current_position.y % height
-        current_position = Point(x, y)
-
-        cell: Contents = grid[current_position.y][current_position.x]
-
-        match cell:
-            case Contents.Air:
-                last_viable_position = current_position
-                num_steps_taken += 1
-            case Contents.Wall:
-                return last_viable_position
-            case Contents.Empty:
-                pass
-
-    return last_viable_position
+    return Grove(grid, instructions)
 
 
 def part_1(raw: str):
-    grid, instructions = parse_input(raw)
-
-    start_x = grid[0].index(Contents.Air)
-    current_position = Point(start_x, 0)
-
-    directions = collections.deque(
-        [
-            Directions.East,
-            Directions.South,
-            Directions.West,
-            Directions.North,
-        ]
-    )
-
-    for instruction in instructions:
-        match instruction:
-            case int():
-                current_position = move(
-                    grid, current_position, directions[0].value, instruction
-                )
-            case "R":
-                directions.rotate(-1)
-            case "L":
-                directions.rotate(1)
-
-    facing_points = {
-        Directions.East: 0,
-        Directions.South: 1,
-        Directions.West: 2,
-        Directions.North: 3,
-    }[directions[0]]
-
-    return (
-        (current_position.y + 1) * 1_000 + (current_position.x + 1) * 4 + facing_points
-    )
+    return parse_input(raw).do_thing()
 
 
 if __name__ == "__main__":
--- aoc_2022/tests/test_day22.py
+++ aoc_2022/tests/test_day22.py
@@ -17,6 +17,46 @@ EXAMPLE = """
 10R5L5R10L4R5L5
 """
 
+"""
+     1111 2222
+     1111 2222
+     1111 2222
+     
+     3333
+     3333
+     3333
+    
+4444 5555
+4444 5555
+4444 5555
+
+6666
+6666
+6666
+
+"""
+
+EXAMPLE_2 = """
+    ...#.#..
+    .#......
+    #.....#.
+    ........
+    ...#
+    #...
+    ....
+    ..#.
+..#....#
+........
+.....#..
+........
+#...
+..#.
+....
+....
+
+10R5L5R10L4R5L5
+"""
+
 
 def test_parse_grid():
     grid, _ = parse_input(EXAMPLE)
@@ -24,12 +64,44 @@ def test_parse_grid():
     assert fifth_row[:4] == [Contents.Air, Contents.Air, Contents.Air, Contents.Wall]
 
 
+def test_edge_size():
+    grove = parse_input(EXAMPLE_2)
+    assert grove.edge_size == 4
+
+
+def test_square_id():
+    grove = parse_input(EXAMPLE_2)
+
+    assert grove.square_id(Point(5, 3)) == 1
+
+    unique_ids = set()
+    for y, line in enumerate(grove.grid):
+        for x, contents in enumerate(line):
+            if contents != Contents.Empty:
+                unique_ids.add(grove.square_id(Point(x, y)))
+
+    assert unique_ids == {1, 2, 3, 4, 5, 6}
+
+
+def test_initial_position():
+    grove = parse_input(EXAMPLE_2)
+    assert grove.current_position == Point(4, 0)
+
+
+def test_peek_step():
+    grove = parse_input(EXAMPLE_2)
+
+    assert grove.peek_step(Dir.South) == (Point(4, 1), Dir.South)
+    assert grove.peek_step(Dir.East) == (Point(5, 0), Dir.East)
+
+    assert grove.peek_step(Dir.North) == (Point(0, 12), Dir.East)
+    grove.current_position = Point(5, 0)
+    assert grove.peek_step(Dir.North) == (Point(0, 13), Dir.East)
+
+    grove.current_position = Point(4, 1)
+    assert grove.peek_step(Dir.West) == (Point(0, 11), Dir.East)
+
+
 def test_move():
-    grid, _ = parse_input(EXAMPLE)
-
-    point_a = Point(11, 6)
-    assert move(grid, point_a, Directions.East.value, 1) == Point(0, 6)
-    assert move(grid, point_a, Directions.East.value, 10) == Point(1, 6)
-
-    assert move(grid, point_a, Directions.North.value, 1) == Point(11, 5)
-    assert move(grid, point_a, Directions.South.value, 5) == Point(11, 7)
+    grove = parse_input(EXAMPLE_2)
+    assert grove.do_thing() == 10_006
