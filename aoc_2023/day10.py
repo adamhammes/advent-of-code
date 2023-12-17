709b264c0df1940fee555cbb05e81330444812ed
Adam Hammes <adam@hammes.io>
2023-12-17 13:19:38 -0600

    2023 Day 10 Part 02

--- aoc_2023/day10.py
+++ aoc_2023/day10.py
@@ -15,6 +15,13 @@ char_to_grid = {
 }
 
 
+def is_curve_character(c: str) -> bool:
+    return c in "LJ7F"
+
+
+COMPLEMENT = {"J": "F", "F": "J", "L": "7", "7": "L"}
+
+
 class Field:
     grid: Grid
     edges: dict[Point, set[Point]]
@@ -60,5 +67,48 @@ def part_1(raw: str) -> int:
     return max(distances.values())
 
 
+def part_2(raw: str) -> int:
+    field = parse_input(raw)
+    loop_points = {field.start}
+
+    queue = collections.deque([field.start])
+    while queue:
+        current = queue.popleft()
+
+        unexplored_neighbors = [n for n in field.edges[current] if n not in loop_points]
+        for n in unexplored_neighbors:
+            queue.append(n)
+            loop_points.add(n)
+
+    x_max, y_max = max(p.x for p in field.grid), max(p.y for p in field.grid)
+    total = 0
+    for y in range(0, y_max + 1):
+        inside = False
+        last_loop_char = None
+        for x in range(-1, x_max + 1):
+            cur_point = Point(x, y)
+
+            if cur_point in loop_points:
+                loop_char = field.grid[cur_point]
+                if loop_char == "|":
+                    inside = not inside
+                    last_loop_char = None
+                elif is_curve_character(loop_char) and last_loop_char is None:
+                    last_loop_char = loop_char
+                elif (
+                    is_curve_character(loop_char)
+                    and loop_char == COMPLEMENT[last_loop_char]
+                ):
+                    inside = not inside
+                    last_loop_char = None
+                elif is_curve_character(loop_char):
+                    last_loop_char = None
+            elif cur_point not in loop_points and inside:
+                total += 1
+
+    return total
+
+
 if __name__ == "__main__":
     print(part_1(lib.get_input(10)))
+    print(part_2(lib.get_input(10)))
